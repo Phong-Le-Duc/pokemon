19be8e7891528aaa75f6907c80e1d4691b6e1178
Phong <phongleduc@gmail.com>
2025-02-27 14:03:44 +0100

    change added

--- home_layout.js
+++ home_layout.js
@@ -1,6 +1,28 @@
 
 // ----------------- BEDRE UDGAVE---------------
 
+
+let divElm = document.createElement("div")
+divElm.id = "root"
+
+divElm.innerHTML = `
+<header>
+    <img class="poke_ball" src="img/pokeball.png" alt="">
+<span class="brand ">Pokédex</span> <br>
+<input class="poke_search" type="text" placeholder="Search" id=""/>
+</header>
+<main></main>
+<footer>created 2025</footer>
+`
+
+document.querySelector("body").append(divElm)
+
+
+
+
+
+
+
 /**
  * Extract id as string from url to pokemon
  * @param {string} pokemonUrl - a url to a pokemon from pokeApi 
